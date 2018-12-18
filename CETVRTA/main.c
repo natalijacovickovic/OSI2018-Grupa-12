2c3d629b160c1e889c0dc3ce9c38deeeb8b1989d
Aleksandra Ševa <aleksandra.seva97@gmail.com>
2018-12-18 17:48:37 +0100

    Pocetna strana za cetvrtu igru

---
+++ CETVRTA/main.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <Windows.h>
+
+
+void odbrojavanje()
+{
+    FILE *dat;
+    int i;
+    if((dat=fopen("tri.txt","r"))!=NULL)
+    {
+         while((i=fgetc(dat))!=EOF)
+            printf("%c",i);
+        fclose(dat);
+    }
+    Sleep(1000);
+    system("cls");
+
+    if((dat=fopen("dva.txt","r"))!=NULL)
+    {
+         while((i=fgetc(dat))!=EOF)
+            printf("%c",i);
+        fclose(dat);
+    }
+    Sleep(1000);
+    system("cls");
+
+    if((dat=fopen("jedan.txt","r"))!=NULL)
+    {
+         while((i=fgetc(dat))!=EOF)
+            printf("%c",i);
+        fclose(dat);
+    }
+    Sleep(1000);
+    system("cls");
+}
+
+void naziv()
+{
+    FILE *dat;
+    int i;
+    if((dat=fopen("cetvrta.txt","r"))!=NULL)
+    {
+        while((i=fgetc(dat))!=EOF)
+        {
+            printf("%c",i);
+        }
+        fclose(dat);
+    }
+}
+
+int main()
+{
+    system("color C0");
+    naziv();
+    Sleep(3000);
+    system("cls");
+    odbrojavanje();
+    return 0;
+}
+
